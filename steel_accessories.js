95d3c9113023d7dde8cee6c3aae03fc694612ea3
Ju Han Kim <jhkim1130@ysacc.co.kr>
2026-08-07 17:41:01 +0900

    feat: stack 0.5m panel section at top for 2.5mH, 3.5mH, 4.5mH (bump v4.40.76)

--- steel_accessories.js
+++ steel_accessories.js
@@ -78,7 +78,7 @@
     }
   };
 
-  const LAYOUT_URL = "steel_accessories_layout.json?v=4.40.75_1786089787835";
+  const LAYOUT_URL = "steel_accessories_layout.json?v=4.40.76_1786092023467";
   const STORAGE_KEY = "water_tank_steel_accessories_layout_v1";
   const FIRESTORE_DOC = "steelAccessoriesLayout";
 
