0662dd721bdcfa390e83875a6f8c16689c184d3c
Ju Han Kim <jhkim1130@ysacc.co.kr>
2026-08-07 22:14:31 +0900

    feat: refine 4.5mH and 5.0mH CS position mappings (CS6-CS8) to match reference diagrams (bump v4.40.80)

--- steel_accessories.js
+++ steel_accessories.js
@@ -78,7 +78,7 @@
     }
   };
 
-  const LAYOUT_URL = "steel_accessories_layout.json?v=4.40.79_1786108358921";
+  const LAYOUT_URL = "steel_accessories_layout.json?v=4.40.80_1786108435554";
   const STORAGE_KEY = "water_tank_steel_accessories_layout_v1";
   const FIRESTORE_DOC = "steelAccessoriesLayout";
 
