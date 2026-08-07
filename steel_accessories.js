ae6102a4aacd851cf8b237409d682f4527223d38
Ju Han Kim <jhkim1130@ysacc.co.kr>
2026-08-07 17:03:40 +0900

    feat: split side panel grids into 1m and 0.5m sections from 1.5mH to 5.0mH (bump v4.40.75)

--- steel_accessories.js
+++ steel_accessories.js
@@ -78,7 +78,7 @@
     }
   };
 
-  const LAYOUT_URL = "steel_accessories_layout.json?v=4.40.74_1786088249340";
+  const LAYOUT_URL = "steel_accessories_layout.json?v=4.40.75_1786089787835";
   const STORAGE_KEY = "water_tank_steel_accessories_layout_v1";
   const FIRESTORE_DOC = "steelAccessoriesLayout";
 
