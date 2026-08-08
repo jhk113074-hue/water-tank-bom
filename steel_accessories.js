6a685c5ec39547583e3144e0c0682a494599dee1
Ju Han Kim <jhkim1130@ysacc.co.kr>
2026-08-08 22:42:13 +0900

    bump: Release v4.40.347 with visual config fix merged to main

--- steel_accessories.js
+++ steel_accessories.js
@@ -78,7 +78,7 @@
     }
   };
 
-  const LAYOUT_URL = "steel_accessories_layout.json?v=4.40.346_1786195637973";
+  const LAYOUT_URL = "steel_accessories_layout.json?v=4.40.347_1786196516196";
   const STORAGE_KEY = "water_tank_steel_accessories_layout_v1";
   const FIRESTORE_DOC = "steelAccessoriesLayout";
 
