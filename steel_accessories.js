e45e72ad1c5399dbf779c7cf16996c3efb402710
Ju Han Kim <jhkim1130@ysacc.co.kr>
2026-08-08 21:30:20 +0900

    feat(parts_db): Add 30 custom steel skid part numbers (WFF-2070ASZN, WFF-2060CSZN, WFF-1140ASZ, WFF-2640ASZ, WFF-1570ASZR/L, etc.) to PART MASTER DB and sync Firestore DB, bump version to 4.40.334

--- steel_accessories.js
+++ steel_accessories.js
@@ -78,7 +78,7 @@
     }
   };
 
-  const LAYOUT_URL = "steel_accessories_layout.json?v=4.40.333_1786192136767";
+  const LAYOUT_URL = "steel_accessories_layout.json?v=4.40.334_1786192202765";
   const STORAGE_KEY = "water_tank_steel_accessories_layout_v1";
   const FIRESTORE_DOC = "steelAccessoriesLayout";
 
