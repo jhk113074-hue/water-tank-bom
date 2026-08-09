fe7b96a178c5b9ad881cc42f70b93d70997a63da
Ju Han Kim <jhkim1130@ysacc.co.kr>
2026-08-09 18:43:16 +0900

    fix: Bind data-tab=tab-project-manager to sidebar button for clean tab rendering v4.40.367

--- steel_accessories.js
+++ steel_accessories.js
@@ -78,7 +78,7 @@
     }
   };
 
-  const LAYOUT_URL = "steel_accessories_layout.json?v=4.40.366_1786268447242";
+  const LAYOUT_URL = "steel_accessories_layout.json?v=4.40.367_1786268584922";
   const STORAGE_KEY = "water_tank_steel_accessories_layout_v1";
   const FIRESTORE_DOC = "steelAccessoriesLayout";
 
