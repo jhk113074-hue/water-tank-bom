0deff40822f754e28c59798bb5085bf8c0c0180f
Ju Han Kim <jhkim1130@ysacc.co.kr>
2026-08-09 18:18:27 +0900

    feat: Move PART MASTER DB and COSTING to main top-level navigation menu v4.40.364

--- steel_accessories.js
+++ steel_accessories.js
@@ -78,7 +78,7 @@
     }
   };
 
-  const LAYOUT_URL = "steel_accessories_layout.json?v=4.40.363_1786265530980";
+  const LAYOUT_URL = "steel_accessories_layout.json?v=4.40.364_1786267096266";
   const STORAGE_KEY = "water_tank_steel_accessories_layout_v1";
   const FIRESTORE_DOC = "steelAccessoriesLayout";
 
