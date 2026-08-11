d2217c90b181928796a1f002301d877364185e16
Ju Han Kim <jhkim1130@ysacc.co.kr>
2026-08-11 21:27:13 +0900

    feat(reinf-audit): populate default data and heightSpecs for EXT(GenSide) and EXT(GenPart) categories v4.40.541

--- steel_accessories.js
+++ steel_accessories.js
@@ -78,7 +78,7 @@
     }
   };
 
-  const LAYOUT_URL = "steel_accessories_layout.json?v=4.40.540_1786450977370";
+  const LAYOUT_URL = "steel_accessories_layout.json?v=4.40.541_1786451187089";
   const STORAGE_KEY = "water_tank_steel_accessories_layout_v1";
   const FIRESTORE_DOC = "steelAccessoriesLayout";
 
