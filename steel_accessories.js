776251ca532041e79cbf278b2f17d0d8d7bb8e05
Ju Han Kim <jhkim1130@ysacc.co.kr>
2026-08-11 21:23:43 +0900

    feat(reinf-audit): add Tab 5 EXT(GenSide) and Tab 6 EXT(GenPart) for 1-5mH in steel_accessories_layout.json v4.40.540

--- steel_accessories.js
+++ steel_accessories.js
@@ -78,7 +78,7 @@
     }
   };
 
-  const LAYOUT_URL = "steel_accessories_layout.json?v=4.40.539_1786450768749";
+  const LAYOUT_URL = "steel_accessories_layout.json?v=4.40.540_1786450977370";
   const STORAGE_KEY = "water_tank_steel_accessories_layout_v1";
   const FIRESTORE_DOC = "steelAccessoriesLayout";
 
