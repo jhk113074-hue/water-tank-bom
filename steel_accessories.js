50c4d2f6292e8b8cac798d8475ec4be35cad1a56
Ju Han Kim <jhkim1130@ysacc.co.kr>
2026-08-11 19:55:58 +0900

    revert(cad-blueprint): revert CAD blueprint tab to restore clean original UI v4.40.533

--- steel_accessories.js
+++ steel_accessories.js
@@ -78,7 +78,7 @@
     }
   };
 
-  const LAYOUT_URL = "steel_accessories_layout.json?v=4.40.532_1786445254422";
+  const LAYOUT_URL = "steel_accessories_layout.json?v=4.40.533_1786445692331";
   const STORAGE_KEY = "water_tank_steel_accessories_layout_v1";
   const FIRESTORE_DOC = "steelAccessoriesLayout";
 
