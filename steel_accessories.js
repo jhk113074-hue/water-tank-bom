311322583ab8cbd63b4bc15b1cb7f7657092b052
Ju Han Kim <jhkim1130@ysacc.co.kr>
2026-08-25 10:57:36 +0900

    chore: bump version to 4.40.676 and sync Firestore DB

--- steel_accessories.js
+++ steel_accessories.js
@@ -78,7 +78,7 @@
     }
   };
 
-  const LAYOUT_URL = "steel_accessories_layout.json?v=4.40.675_1787621156977";
+  const LAYOUT_URL = "steel_accessories_layout.json?v=4.40.676_1787623032185";
   const STORAGE_KEY = "water_tank_steel_accessories_layout_v1";
   const FIRESTORE_DOC = "steelAccessoriesLayout";
 
