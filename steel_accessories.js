03acccebc443e4c5b2fa4de5a50ce790df915a53
Ju Han Kim <jhkim1130@ysacc.co.kr>
2026-08-25 21:38:20 +0900

    chore: bump version to 4.40.678 after merging joint-bolt decoupling fixes

--- steel_accessories.js
+++ steel_accessories.js
@@ -78,7 +78,7 @@
     }
   };
 
-  const LAYOUT_URL = "steel_accessories_layout.json?v=4.40.677_1787634347963";
+  const LAYOUT_URL = "steel_accessories_layout.json?v=4.40.678_1787661466128";
   const STORAGE_KEY = "water_tank_steel_accessories_layout_v1";
   const FIRESTORE_DOC = "steelAccessoriesLayout";
 
