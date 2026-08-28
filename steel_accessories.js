350bdf6e3de0437a91ccd397400c8a35b4cc3316
Ju Han Kim <jhkim1130@ysacc.co.kr>
2026-08-28 20:28:02 +0900

    Update Panel Part No mode dropdown text to English With/Without Drilling Spec (v=4.40.716)

--- steel_accessories.js
+++ steel_accessories.js
@@ -78,7 +78,7 @@
     }
   };
 
-  const LAYOUT_URL = "steel_accessories_layout.json?v=4.40.703_1787836712428";
+  const LAYOUT_URL = "steel_accessories_layout.json?v=4.40.715_1787841408982";
   const STORAGE_KEY = "water_tank_steel_accessories_layout_v1";
   const FIRESTORE_DOC = "steelAccessoriesLayout";
 
